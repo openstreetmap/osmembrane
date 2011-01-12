785c5c1d9f925e5376c3d527ec21999ba64268f9
Tobias Kuhn <tobias@osmembrane.de>
2011-01-12 11:53:33 +0000

    paar Changes in der TI-VL

--- src/de/osmembrane/view/panels/LibraryPanelGroup.java
+++ src/de/osmembrane/view/panels/LibraryPanelGroup.java
@@ -1,15 +1,48 @@
 package de.osmembrane.view.panels;
 
-import javax.swing.JPanel;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.border.EtchedBorder;
+
+import de.osmembrane.model.AbstractFunction;
 import de.osmembrane.model.AbstractFunctionGroup;
 
 public class LibraryPanelGroup extends JPanel {
 	
+	/**
+	 * The id this panel group has in its main Library panel
+	 * (used for click handling calls)
+	 */
 	private int id;
 	
+	/**
+	 * The header button of this panel group that can make it
+	 * expandable or contractable
+	 */
+	private JButton headerButton;
+	
 	public LibraryPanelGroup(AbstractFunctionGroup afg) {
-		// TODO Auto-generated constructor stub
+		setLayout(new GridBagLayout());
+		setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.insets = new Insets(8, 8, 8, 8);
+		
+		headerButton = new JButton();
+		headerButton.setText(afg.getFriendlyName());
+		add(headerButton);
+		
+		for (AbstractFunction af : afg.getFunctions()) {
+			
+		}
 	}
 
 	public void setId(int id) {
