5d33c60b7f1bbd9c44e4aeccce3950fa8b059869
Jakob Jarosch <jakob@osmembrane.de>
2011-01-18 08:59:32 +0000

    Connector-Farben hinzugefügt.

--- src/de/osmembrane/model/pipeline/ConnectorType.java
+++ src/de/osmembrane/model/pipeline/ConnectorType.java
@@ -1,17 +1,20 @@
 package de.osmembrane.model.pipeline;
 
+import java.awt.Color;
+
 public enum ConnectorType {
-	CHANGE ("Change", -1),
-	ENTITY ("Entity", -1),
-	DATASET ("Dataset", 1);
+	CHANGE ("Change", -1, new Color(255,0,0)),
+	ENTITY ("Entity", -1, new Color(0,255,0)),
+	DATASET ("Dataset", 1, new Color(0,0,255));
 	
 	private String friendlyName;
 	private int maxConnections;
+	private Color color;
 	
-	
-	private ConnectorType(String friendlyName, int maxConnections) {
+	private ConnectorType(String friendlyName, int maxConnections, Color color) {
 		this.friendlyName = friendlyName;
 		this.maxConnections = maxConnections;
+		this.color = color;
 	}
 	
 	public String getFriendlyName() {
@@ -20,6 +23,10 @@ public enum ConnectorType {
 	
 	public int getMaxConnections() {
 		return maxConnections;
+	}
+	
+	public Color getColor() {
+		return color;
 	}
 	
 	public static ConnectorType parseString(String type) {
