254f2d23bdf5a5b43b52f360897db7d6707bc94c
Tobias Kuhn <tobias@osmembrane.de>
2011-01-28 08:54:04 +0000

    BBox-Dialog Skizze

--- src/de/osmembrane/view/dialogs/BoundingBoxDialog.java
+++ src/de/osmembrane/view/dialogs/BoundingBoxDialog.java
@@ -1,7 +1,63 @@
 package de.osmembrane.view.dialogs;
 
-import de.osmembrane.view.AbstractDialog;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
 
-public class BoundingBoxDialog extends AbstractDialog {
+import de.osmembrane.view.AbstractDialog;
+import de.osmembrane.view.IView;
+import de.unistuttgart.iev.osm.bboxchooser.BBoxChooserDialog;
+
+/**
+ * 
+ * Simple dialog to display the generated command line, export it, or copy it to
+ * the clipboard.
+ * 
+ * @see Spezifikation.pdf, chapter 2.4
+ * 
+ * @author tobias_kuhn
+ *
+ */
+public class BoundingBoxDialog implements IView {
+
+	private static final long serialVersionUID = 5182327519016989905L;
+	
+	private BBoxChooserDialog dialog;
+	
+	/**
+	 * Creates a new {@link BoundingBoxDialog}
+	 */
+	public BoundingBoxDialog() {
+		dialog = new BBoxChooserDialog();
+		dialog.setModal(true);
+	}
+
+	@Override
+	public void showWindow() {
+		dialog.setVisible(true);
+	}
+
+	@Override
+	public void hideWindow() {
+		dialog.setVisible(false);
+	}
+
+	@Override
+	public void setWindowTitle(String viewTitle) {
+		dialog.setTitle(viewTitle);
+	}
+
+	@Override
+	public void centerWindow() {
+		Point screenCenter = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
+		Point edgeLeftTop = new Point(screenCenter.x - (dialog.getWidth() / 2),
+									  screenCenter.y - (dialog.getHeight() / 2));
+		dialog.setLocation(edgeLeftTop.x, edgeLeftTop.y);
+	}
+
+	@Override
+	public void bringToFront() {
+		dialog.toFront();
+	}
+
 
 }
