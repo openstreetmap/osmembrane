044036279768f12294e46cd7fdd737edfa80bbaf
Jakob Jarosch <jakob@osmembrane.de>
2011-01-30 22:17:14 +0000

    Zoom-Standard nach New.

--- src/de/osmembrane/controller/actions/NewPipelineAction.java
+++ src/de/osmembrane/controller/actions/NewPipelineAction.java
@@ -9,10 +9,12 @@ import javax.swing.Action;
 import javax.swing.JOptionPane;
 import javax.swing.KeyStroke;
 
+import de.osmembrane.controller.ActionRegistry;
 import de.osmembrane.model.ModelProxy;
 import de.osmembrane.resources.Resource;
 import de.osmembrane.tools.I18N;
 import de.osmembrane.tools.IconLoader.Size;
+import de.osmembrane.view.actions.StandardViewAction;
 
 /**
  * Action to create a completely empty, new pipeline.
@@ -29,8 +31,10 @@ public class NewPipelineAction extends AbstractAction {
 	 */
 	public NewPipelineAction() {
 		putValue(Action.NAME, "New Pipeline");
-		putValue(Action.SMALL_ICON, Resource.PROGRAM_ICON.getImageIcon("new_pipeline.png", Size.SMALL));
-		putValue(Action.LARGE_ICON_KEY, Resource.PROGRAM_ICON.getImageIcon("new_pipeline.png", Size.NORMAL));
+		putValue(Action.SMALL_ICON, Resource.PROGRAM_ICON.getImageIcon(
+				"new_pipeline.png", Size.SMALL));
+		putValue(Action.LARGE_ICON_KEY, Resource.PROGRAM_ICON.getImageIcon(
+				"new_pipeline.png", Size.NORMAL));
 		putValue(Action.ACCELERATOR_KEY, KeyStroke.getKeyStroke(KeyEvent.VK_N,
 				Toolkit.getDefaultToolkit().getMenuShortcutKeyMask()));
 		putValue(Action.SHORT_DESCRIPTION, "News an add pipeline item.");
@@ -48,5 +52,8 @@ public class NewPipelineAction extends AbstractAction {
 			}
 		}
 		ModelProxy.getInstance().accessPipeline().clear();
+
+		ActionRegistry.getInstance().get(StandardViewAction.class)
+				.actionPerformed(null);
 	}
 }
