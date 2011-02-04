f0241aef00fd77494a454d0084b19dda2883707d
Jakob Jarosch <jakob@osmembrane.de>
2011-02-04 20:26:22 +0000

    Interfaces for the ExecutionStateDialog

--- src/de/osmembrane/controller/ICallable.java
+++ src/de/osmembrane/controller/ICallable.java
@@ -1,6 +1,16 @@
 package de.osmembrane.controller;
 
+/**
+ * Like a very simple observer.
+ * 
+ * @author jakob_jarosch
+ */
 public interface ICallable {
 
+	/**
+	 * Interacts like a observer.
+	 * 
+	 * @param obj the object which called this method
+	 */
 	public void callback(Object obj);
 }
--- src/de/osmembrane/view/interfaces/IExecutionStateDialog.java
+++ src/de/osmembrane/view/interfaces/IExecutionStateDialog.java
@@ -9,11 +9,42 @@ import de.osmembrane.controller.ICallable;
  */
 public interface IExecutionStateDialog {
 	
-	public void addLine(String outputLine);
+	/**
+	 * Sets the status of the execution action.
+	 * 
+	 * @param status status to be set
+	 */
+	public void setStatus(String status);
 	
-	public void dispose();
+	/**
+	 * Sets the progress of the bar.
+	 * 
+	 * @param progress progress to be set
+	 */
+	public void setProgress(double progress);
 	
+	/**
+	 * Adds a line to the output.
+	 * 
+	 * @param outputLine line which should be added
+	 */
+	public void addOutputLine(String outputLine);
+	
+	/**
+	 * Clears the window.
+	 */
 	public void clear();
 	
+	/**
+	 * Closes the window.
+	 */
+	public void dispose();
+	
+	/**
+	 * Adds a callback object, which is called if the window is closed.<br/>
+	 * Callback should be replied with the instance-object of the dialog.
+	 * 
+	 * @param callable which should be called if the windows was closed
+	 */
 	public void setCallbackObject(ICallable callable);
 }
