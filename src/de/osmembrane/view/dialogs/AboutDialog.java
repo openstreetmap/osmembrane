7b7291fc71e918da15808f3e9ff4b08946826f4b
Tobias Kuhn <tobias@osmembrane.de>
2011-02-06 18:34:42 +0000

    fixes #85 oder so

--- src/de/osmembrane/view/dialogs/AboutDialog.java
+++ src/de/osmembrane/view/dialogs/AboutDialog.java
@@ -1,7 +1,62 @@
 package de.osmembrane.view.dialogs;
 
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JEditorPane;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+
+import de.osmembrane.resources.Resource;
+import de.osmembrane.tools.I18N;
+import de.osmembrane.tools.IconLoader.Size;
 import de.osmembrane.view.AbstractDialog;
 
+/**
+ * The about dialog informing people about the program.
+ * 
+ * @author tobias_kuhn
+ * 
+ */
 public class AboutDialog extends AbstractDialog {
+
+	private static final long serialVersionUID = 525351301396477062L;
+
+	/**
+	 * Creates a new {@link AboutDialog}.
+	 */
+	public AboutDialog() {
+		setLayout(new BorderLayout());
+		
+		JLabel splash = new JLabel(new ImageIcon(this.getClass().getResource(
+				"/de/osmembrane/resources/images/splash.png")));
+		add(splash, BorderLayout.NORTH);
+		
+		JEditorPane infoText = new JEditorPane();
+		infoText.setContentType("text/html");
+		infoText.setEditable(false);
+		infoText.setText(I18N.getInstance().getString("View.AboutDialog.Info"));		
+		add(infoText, BorderLayout.CENTER);
+
+		JButton okButton = new JButton(I18N.getInstance().getString("View.OK"));
+		okButton.addKeyListener(returnButtonListener);
+		okButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				hideWindow();
+			}
+		});
+		add(okButton, BorderLayout.SOUTH);
+		
+		setTitle(I18N.getInstance().getString("View.AboutDialog"));
+
+		pack();
+		centerWindow();
+	}
 
 }
